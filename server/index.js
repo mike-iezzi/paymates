0756cddebd4ddf5cd4cb7ebc7414476a21c63973
Michael Hallee <mdhallee@princeton.edu>
2021-04-25 19:43:08 -0400

    backend server responding

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+// const pool = require("./db");
+
+// //middleware
+// app.use(cors());
+// app.use(express.json()); //req.body
+
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
+
+app.get("/", async (req, res) => {
+      res.send("ayooo0000000000");
+  });
