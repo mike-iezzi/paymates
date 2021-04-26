8afc6b6d50811256c70d12a013ec2680dc622d38
Michael Hallee <mdhallee@princeton.edu>
2021-04-25 23:13:31 -0400

    added groups and table relationships

--- server/database.sql
+++ server/database.sql
@@ -5,22 +5,29 @@ CREATE TABLE deals(
     description VARCHAR(128) NOT NULL,
     owner_user_id SERIAL NOT NULL,
     amount NUMERIC(10,2) NOT NULL,
-    method VARCHAR(32) NOT NULL
+    method VARCHAR(32) NOT NULL,
+    transactions_list SERIAL []
 );
 
 CREATE TABLE transactions(
     transaction_id SERIAL NOT NULL PRIMARY KEY,
-    deal_id SERIAL NOT NULL,
     user_id SERIAL NOT NULL,
+    deal_id SERIAL NOT NULL,
+    description VARCHAR(128) NOT NULL
     amount_owed NUMERIC(10,2) NOT NULL,
     amount_paid NUMERIC(10,2) NOT NULL,  
-    description VARCHAR(128)
 );
 
 CREATE TABLE users(
     user_id SERIAL NOT NULL PRIMARY KEY,
-    firstname VARCHAR(32) NOT NULL,
-    lastname VARCHAR(32) NOT NULL,
+    username VARCHAR(32) NOT NULL,
+    firstname VARCHAR(32),
+    lastname VARCHAR(32),
     transaction_list SERIAL [],
     groups_list SERIAL []
+);
+
+CREATE TABLE groups(
+    group_id SERIAL NOT NULL PRIMARY KEY,
+    users_list SERIAL [],
 );
