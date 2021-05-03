0291bb9f2957b8f121b80c5814dae4cdec5e947a
Michael Hallee <mdhallee@princeton.edu>
2021-05-03 01:48:19 -0400

    authentication base

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Deal from "./components/Deal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// authentication packages
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.signInWithPhoneNumber(),
+};
+
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+type providers = {
+  signInWithGoogle: () => void;
+  signInWithPhoneNumber: (
+    phoneNumber: string,
+    applicationVerifier: firebase.auth.ApplicationVerifier,
+  ) => void;
+};
+
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Switch>
+//           <Route path={"/login"}>
+//             <Login />
+//           </Route>
+//           <Route path={"/book-list"}>
+//             <BookList />
+//           </Route>
+//         </Switch>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyBuAUeTnJbqbs-SNa1fh2QcaLaEwQkLYQ8",
+    authDomain: "paymates-7b09f.firebaseapp.com",
+    projectId: "paymates-7b09f",
+    storageBucket: "paymates-7b09f.appspot.com",
+    messagingSenderId: "56379249650",
+    appId: "1:56379249650:web:acdb7a74b3d42a922d516b"
+  };
+export default config;
